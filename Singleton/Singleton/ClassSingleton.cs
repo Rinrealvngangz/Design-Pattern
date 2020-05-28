9aa8f5f2a3fdc0e8ab4360541abe608bd1e7bcea
Tuan <Rinrealvngangz.com>
2020-05-29 03:43:06 +0700

    Singleton

---
+++ Singleton/Singleton/ClassSingleton.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+   public class ClassSingleton
+    {
+        private static ClassSingleton instance;
+        public int number { get; set; }
+        private static readonly object lockobj = new object();
+        
+        public ClassSingleton(int number)
+        {
+            this.number = number;
+        }
+
+        public static ClassSingleton GetInstance()
+        {
+            if (instance ==null)
+            {
+                lock (lockobj)
+                {
+                    if (instance == null)
+                    {
+                        Random ran = new Random();
+                        instance = new ClassSingleton(ran.Next(1, 5));
+                    }
+                }
+              
+           }
+          
+            return instance;
+        }
+
+        public void helloSingleton()
+        {
+            Console.WriteLine("I'm Singleton num {0}",number);
+        }
+
+        
+    }
+}
---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var singelton = new Thread(()=> ClassSingleton.GetInstance().helloSingleton());
+
+            var singelton1 = new Thread(() => ClassSingleton.GetInstance().helloSingleton());
+
+            singelton.Start();
+            singelton1.Start();
+          
+          
+        }
+    }
+}
